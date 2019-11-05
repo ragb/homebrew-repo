c7e31ce5be565a8506875276bb649e13ee5a5139
Rui Batista <ruiandrebatista@gmail.com>
2019-11-05 11:58:20 +0000

    Update pcaudiolib link.

--- formula/pcaudiolib.rb
+++ formula/pcaudiolib.rb
@@ -1,7 +1,7 @@
 class Pcaudiolib < Formula
   desc "Portable C Audio Library"
   homepage "https://github.com/espeak-ng/pcaudiolib"
-  url "https://github.com/espeak-ng/pcaudiolib/releases/download/1.1/pcaudiolib-1.1.tar.gz"
+  url "https://github.com/espeak-ng/pcaudiolib/archive/1.1.tar.gz"
   sha256 "1594aed0cc6d0df181088cc5314da54f9a049358755b6ed0c07c1778cf4c8487"
   revision 1
 
